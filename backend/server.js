977ca2c5cb513fc18695142a112772dceb37e201
Vanshika <vanshikachhabra1407@gmail.com>
2026-03-14 17:18:07 +0530

    craeted route level  middleware in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const foodRoutes = require("./routes/foodRoutes");
+const pickupRoutes = require("./routes/pickupRoutes");
+
+const app = express();
 
 await connectDB();
-const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Food Waste Redistribution API running");
-});
-
-const PORT = process.env.PORT || 5000;
-
-const authRoutes = require("./routes/authRoutes");
-const foodRoutes = require("./routes/foodRoutes");
-const pickupRoutes = require("./routes/pickupRoutes");
 app.use("/api/auth", authRoutes);
 app.use("/api/food", foodRoutes);
 app.use("/api/pickup", pickupRoutes);
 
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
