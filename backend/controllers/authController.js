e65c83283a4c6faf082074368f07286274e0d632
Vanshika <vanshikachhabra1407@gmail.com>
2026-03-14 17:13:39 +0530

    Created login api in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -30,3 +30,35 @@ exports.register = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
